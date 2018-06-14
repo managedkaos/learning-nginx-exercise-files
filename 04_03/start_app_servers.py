5481471edcb07666128f448b106a646a2f1c6f0d
Michael <michael@managedkaos.com>
2018-06-14 15:57:31 -0700

    Move to dynamic app server

--- 04_03/start_app_servers.py
+++ 04_03/start_app_servers.py
@@ -1,70 +1,55 @@
 #!/usr/bin/env python3
 '''Module: Starts three HTTP servers'''
 import os
-import socket
-import http.server
-import socketserver
-from pathlib import Path
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from pprint import pprint
+
+hostName = "localhost"
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        #for attr in dir(self):
+        #    print("self.%s = %r" % (attr, getattr(self, attr)))
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("""
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <style> h1 {
+                            font-size:200px;
+                            text-align:center;
+                            margin-left:auto;
+                            margin-right:auto
+                           }
+                </style>
+                <title>%s</title>
+            </head>
+        <body>""" % "PORT" , "utf-8"))
+        self.wfile.write(bytes("<body><h1>PORT</h1>", "utf-8"))
+        self.wfile.write(bytes("<p>You accessed path: {}</p>".format(self.path), "utf-8"))
+        self.wfile.write(bytes("<p>{}</p>".format(time.asctime()), "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+def start_server(port):
+    myServer = HTTPServer((hostName, port), MyServer)
+    print(time.asctime(), "Server Starts - http://%s:%s" % (hostName, port))
+
+    try:
+        myServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    myServer.server_close()
+    print(time.asctime(), "Server Stops - http://%s:%s" % (hostName, port))
 
 # list of the ports the servers will listen on
 PORTS = [7001, 7002, 7003]
 
 # list to hold the PIDs from the forked servers
 SERVERS = []
-
-# setup a 1 second timeout for the ports
-SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SOCK.settimeout(1)
-
-# class for setting up socket reuse
-class MyTCPServer(socketserver.TCPServer):
-    '''Class: setups up socket reuse'''
-    def server_bind(self):
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind(self.server_address)
-
-# function for starting the server
-def start_server(server_port):
-    '''Function: Writes an index file and starts the server'''
-    server_root = '/tmp/server_' + str(server_port)
-    server_index = server_root + '/index.html'
-
-    # create the path to the server root if it does not exist
-    Path(server_root).mkdir(parents=True, exist_ok=True)
-
-    # write the HTML to the index file
-    index = open(server_index, 'w')
-    index.write("""
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <style> h1 {
-                        font-size:200px;
-                        text-align:center;
-                        margin-left:auto;
-                        margin-right:auto
-                       }
-            </style>
-        </head>
-        <body>
-            <h1>"""+str(server_port)+"""</h1>
-        </body>
-    </html>
-    """)
-    index.close()
-
-    # create a handler for the HTTP server
-    handler = http.server.SimpleHTTPRequestHandler
-
-    # start/stop the HTTP server
-    with MyTCPServer(("", server_port), handler) as httpd:
-        try:
-            print("starting backend on http://localhost:{}".format(server_port))
-            os.chdir(server_root)
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            httpd.shutdown()
-            print("stopping backend on http://localhost:{}".format(server_port))
 
 # start a fork for each port
 for port in PORTS:
