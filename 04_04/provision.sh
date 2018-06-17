fc2464e8af43fcf4d5642b7263f34dedac555a26
Michael <michael@managedkaos.com>
2018-06-17 16:24:55 -0700

    update provisioning

--- 04_04/provision.sh
+++ 04_04/provision.sh
@@ -13,3 +13,5 @@ cp /vagrant/upstreams.conf /etc/nginx/conf.d
 # Restart and check the status of everything
 systemctl restart nginx
 systemctl status nginx.service --no-pager
+/usr/bin/python3 /vagrant/start_app_servers.py &
+
