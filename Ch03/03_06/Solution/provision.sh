40c5880f1a1956b087689a8185cae10c9116257a
Michael <michael@managedkaos.com>
2018-06-22 12:28:22 -0700

    Create 03_07

--- Ch03/03_06/Solution/provision.sh
+++ Ch03/03_06/Solution/provision.sh
@@ -64,11 +64,3 @@ chmod 600 /etc/nginx/passwords
 openssl req -batch -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/ssl/private/nginx.key \
 	-out /etc/ssl/certs/nginx.crt 2>/dev/null
-
-
-
-
-
-
-
-
